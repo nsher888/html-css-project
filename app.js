aeda5668e9295aa8cac97283a8f83244beb42597
nsher888 <51024662+nsher888@users.noreply.github.com>
2021-12-07 05:18:57 +0400

    overlay

--- app.js
+++ app.js
@@ -3,22 +3,44 @@ const modalBtn = document.querySelector('.modalBtn');
 const closeBtn = document.querySelector('.close');
 
 
+
+const name = document.querySelector('#name')
+const password = document.querySelector('#password')
+const form = document.querySelector('#form')
+const errorElement = document.querySelector('.error')
+const overlay = document.getElementById('overlay');
+
+
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', clickOutside);
+// window.addEventListener('click', clickOutside);
 
 
 function openModal() {
     modal.style.display = 'block';
+    overlay.classList.add('active');
 }
 
 function closeModal() {
     modal.style.display = 'none';
+    overlay.classList.remove('active');
 }
 
-function clickOutside(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
-        console.log('clicked')
+overlay.addEventListener('click', () => {
+    modal.style.display = 'none';
+    overlay.classList.remove('active');
+})
+
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if(name.value === '' || password.value === ''){
+        messages.push('All fields are required!')
     }
-}
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ')
+    }
+})
+
+
